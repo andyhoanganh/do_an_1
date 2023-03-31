18bff82d73d8046a695a09432837f93bc9466908
uyenthanh02 <uyensau3005@gmail.com>
2023-03-31 16:43:11 +0700

    hello

--- do_an_1/login.Designer.cs
+++ do_an_1/login.Designer.cs
@@ -34,31 +34,33 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(88, 110);
+            this.button1.Location = new System.Drawing.Point(78, 88);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(173, 99);
             this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
+            this.button1.Text = "aaaaaa";
             this.button1.UseVisualStyleBackColor = true;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(329, 112);
+            this.label1.Location = new System.Drawing.Point(292, 90);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(51, 20);
+            this.label1.Size = new System.Drawing.Size(44, 16);
             this.label1.TabIndex = 1;
             this.label1.Text = "label1";
             // 
             // login
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.Info;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
-            this.ClientSize = new System.Drawing.Size(539, 364);
+            this.ClientSize = new System.Drawing.Size(479, 291);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "login";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Đăng Nhập";
